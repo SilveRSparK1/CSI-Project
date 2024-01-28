57741c143dfa9b5e20dd512d6c2a2e6089e9eea6
Akhil Manoj <akhil.manoj@hotmail.com>
2024-01-28 22:52:05 +0530

    first commit

---
+++ deploy.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import pickle
+
+app=Flask(__name__)
+#load the model
+model=pickle.load(open("Saved_model.sav", 'rb'))
+
+@app.route('/')
+def home():
+    result = ''
+    return render_template('index.html',**locals())
+
+@app.route('/predict',methods=['POST', 'GET'])
+def predict():
+    sepal_length = float(request.form['SepalLengthCm'])
+    sepal_width = float(request.form['SepalWidthCm'])
+    petal_length = float(request.form['PetalLengthCm'])
+    petal_width = float(request.form['PetalWidthCm'])
+    result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+    return render_template('index.html',**locals())
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
